22db54af646c491905ca214354ecc5e65ff0c9c5
Rodolfo Dassaev Tagle <rodolfo.tagle@therocketcode.com>
2022-07-26 16:12:54 -0500

    add scrollbar styles

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,12 +2,10 @@ import '../assets/scss/sections/Header.scss';
 
 function Header() {
   return (
-    <header>
-      <div className="h-wrapper reiju">
-        <div className="uppercase">About</div>
-        <div className="uppercase">Projects</div>
-        <div className="uppercase">Contact</div>
-      </div>
+    <header className="h-wrapper reiju">
+      <div className="uppercase">About</div>
+      <div className="uppercase">Projects</div>
+      <div className="uppercase">Contact</div>
     </header>
   )
 }
