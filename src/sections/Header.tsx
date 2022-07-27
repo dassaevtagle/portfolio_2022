103850313af9b15b351cf36773a47ec7d03e9311
Rodolfo Dassaev Tagle <rodolfo.tagle@therocketcode.com>
2022-07-27 03:46:36 -0500

    add logo on header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,6 +3,7 @@ import '../assets/scss/sections/Header.scss';
 function Header() {
   return (
     <header className="h-wrapper reiju">
+      <div className="mr-auto ml-4 text-3xl"><span className="text-highlight">T</span>agle</div>
       <div className="uppercase">About</div>
       <div className="uppercase">Projects</div>
       <div className="uppercase">Contact</div>
