67b3e0730299f6a727a8a1c3c73da0531f5a9bf2
Dassaev Tagle <dassaev.tagle@gmail.com>
2022-08-03 12:35:35 -0500

    add hover on externallink, add remarked text

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,15 +1,18 @@
 import { ReactNode, MouseEvent } from "react";
+import '../assets/scss/components/ExternalLink.scss'
 
 type ExternalLinkProps = {
   href: string;
   classes?: string;
+  type?: "hover";
   children: ReactNode;
 }
-function ExternalLink ({href, classes, children}: ExternalLinkProps) {
+
+function ExternalLink ({href, type, classes, children}: ExternalLinkProps) {
   const stopPropagation = (e: MouseEvent<HTMLAnchorElement>) => {
     e.stopPropagation()
   } 
-  return <a href={href} className={classes} onClick={stopPropagation} target="_blank" rel="noopener noreferrer">{children}</a>
+  return <a href={href} className={type ? 'hover-link ' + classes : classes} onClick={stopPropagation} target="_blank" rel="noopener noreferrer">{children}</a>
 }
 
 export default ExternalLink
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,6 +4,7 @@ import ProfileBlack from "../assets/img/memoji_rodolfo.png"
 import {ReactComponent as ExternalIcon} from "../assets/svg/icons/external.svg"
 
 import '../assets/scss/sections/About.scss';
+import ExternalLink from "../components/ExternalLink";
 
 function About () {
   return (
@@ -12,19 +13,20 @@ function About () {
       <div className="about-grid">
         <div className="about-grid_description">
           <p>
-            Hey! I'm Rodolfo Dassaev Tagle, a Software Engineer who loves front-end development.
+            Hey! I'm Rodolfo Dassaev Tagle, a Software Engineer who loves <span className="text-highlight">Front End</span>.
             <br/>
-            I started to learn programming back in 2019 when I found about The Odin Project,
-            since then I've worked as a Fullstack developer using technologies such as .NET, Scala and Node.js.
+            I started to learn programming back in 2019 when I found about&nbsp;
+            <ExternalLink href="https://theodinproject.com" type="hover">The Odin Project</ExternalLink>,
+            since then I've worked as a Full Stack Developer using technologies such as .NET, Scala and Node.js.
             <br/>
-            Currently I focus on mastering my skills in JavaScript and TypeScript.
+            Currently I focus on mastering my skills in <span className="text-highlight">JavaScript</span> and <span className="text-highlight">TypeScript</span>.
           </p>
         </div>
         <div className="about-grid_memoji">
           <img src={ProfileBlack} alt="Me" className="rounded image" />
         </div>
         <div className="about-grid_tech">
-          These are some <span className="underline">technologies</span> I've been working with recently:
+          These are some <span className="underline text-highlight">technologies</span> I've been working with recently:
           <div className="tech-container">
             <ul className="grid grid-cols-3 gap-2 ml-8 list-disc text-lg reiju p-4 gap-x-12">
               <li>React</li>
@@ -37,15 +39,14 @@ function About () {
           </div> 
         </div>
         <div className="about-grid_resume">
-          <a
-            className="rounded py-3 px-6 border-solid border-2 hover:border-highlight hover:text-highlight"
-            href="https://drive.google.com/file/d/1LxeepIkH6VVdJ8SxOMem-RHj03PUii4p/view?usp=sharing"
-            target="_blank" rel="noopener noreferrer">
+          <ExternalLink
+            classes="rounded py-3 px-6 border-solid border-2 hover:border-highlight hover:text-highlight"
+            href="https://drive.google.com/file/d/1LxeepIkH6VVdJ8SxOMem-RHj03PUii4p/view?usp=sharing">
             Resumé
             <span className="inline-flex ml-2 my-auto align-text-bottom">
               <ExternalIcon/>
             </span>
-          </a>
+          </ExternalLink>
         </div>
       </div>
     </FullPageContainer>
